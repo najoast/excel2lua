6b703b6e20bdcc0c6dcd2a28c2d09867c55d0b20
zhouyan <zhouyan@onemt.com.cn>
2019-04-01 19:00:52 +0800

    ok

--- main.go
+++ main.go
@@ -29,18 +29,21 @@ type field struct {
 }
 
 func parseFields(fieldsName []string, fieldsDesc []string) []*field {
+	fmt.Println("parseFields", fieldsName, fieldsDesc)
 	size := len(fieldsName)
 	assert(size == len(fieldsDesc), "size: fieldsName != fieldsDesc")
 	fieldList := make([]*field, size, size)
-	for i := 0; i < len(fieldsName); i++ {
+	for i := 0; i < size; i++ {
 		desc := strings.Split(fieldsDesc[i], "|")
 		f := &field{
-			i,
-			strings.TrimSpace(fieldsName[i]),
-			strings.TrimSpace(desc[0]),
+			_index: i,
+			_name:  strings.TrimSpace(fieldsName[i]),
+			_type:  strings.TrimSpace(desc[0]),
 		}
 		if len(desc) >= 2 {
 			f._attr = strings.TrimSpace(desc[1])
+		} else {
+			f._attr = "nil"
 		}
 		fieldList[i] = f
 	}
@@ -56,13 +59,16 @@ func getDefaultValue(_type string) string {
 	case "string":
 		return "\"\""
 	case "array":
+		return ""
 	case "dict":
-		return "{}"
+		return ""
 	}
-	panic("don't have default value in this type" + _type)
+	fmt.Printf("invalid type[%v]", _type)
+	panic("don't have default value in this type:" + _type)
 }
 
 func cellWrapper(f *field, cell string) string {
+	fmt.Println("cellWrapper", f, cell)
 	if cell == "" {
 		cell = getDefaultValue(f._type)
 	}
@@ -79,26 +85,32 @@ func cellWrapper(f *field, cell string) string {
 	case "dict":
 		// input: "1",10007,5|"2",10007,5
 		// output: {["1"]={10007,5},["2"]={10007,5}}
-		value = "{"
-		for _, item := range strings.Split(cell, "|") {
-			// item: "1",10007,5
-			itemCells := strings.Split(item, ",")
-			size := len(itemCells)
-			if size < 2 {
-				panic(fmt.Sprintf("Invalid dict format: %s", cell))
-			} else if size == 2 {
-				value += fmt.Sprintf("[%v]=%v,", itemCells[0], itemCells[1])
-			} else {
-				value += fmt.Sprintf("[%v]={%v},", itemCells[0], strings.Join(itemCells[1:], ","))
+		if cell == "" {
+			value = "{}"
+		} else {
+			value = "{"
+			for _, item := range strings.Split(cell, "|") {
+				// item: "1",10007,5
+				itemCells := strings.Split(item, ",")
+				fmt.Println("item, itemCells", item, len(item), itemCells, len(itemCells), cell)
+				size := len(itemCells)
+				if size < 2 {
+					panic(fmt.Sprintf("Invalid dict format: %s", cell))
+				} else if size == 2 {
+					value += fmt.Sprintf("[%v]=%v,", itemCells[0], itemCells[1])
+				} else {
+					value += fmt.Sprintf("[%v]={%v},", itemCells[0], strings.Join(itemCells[1:], ","))
+				}
 			}
+			value += "}"
 		}
-		value += "}"
 	case "comment":
 	}
 	return fmt.Sprintf("--[[%s]]%v,", f._name, value)
 }
 
 func processSheet(xlsx *excelize.File, sheetName string) {
+	fmt.Println("Process sheet", sheetName)
 	rows, err := xlsx.GetRows(sheetName)
 	if err != nil {
 		fmt.Println(err)
@@ -110,14 +122,18 @@ func processSheet(xlsx *excelize.File, sheetName string) {
 	}
 	luaCode := "return {\n"
 	fields := parseFields(rows[0], rows[1])
+	fmt.Printf("fields:%v\n", fields)
 
 	for rowIndex, row := range rows {
+		row = row[:len(row)-1]
+		fmt.Println("row:", rowIndex, row, len(row))
 		if rowIndex < 3 {
 			continue
 		}
 		// line := fmt.Sprintf("  --[[%d]]{", rowIndex-2)
 		line := "  {"
 		for colIndex, colCell := range row {
+			fmt.Println("col:", colIndex, colCell)
 			f := fields[colIndex]
 			assert(f != nil, "field not found")
 			if f._type == "comment" {
